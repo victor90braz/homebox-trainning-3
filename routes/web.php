de90dea79a29b22ee251f77d9bb3a619034b5997
victor braz <victor.90braz@gmail.com>
2024-01-19 16:02:29 +0100

    create a addListMember

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function () {
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("76cf69a4f6", [
+        'email_address' => 'victor.90braz@gmail.com',
+        'status' => 'subscribed'
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
