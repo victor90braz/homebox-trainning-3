df41367a966b792c993620252d7f5952334bc122
victor braz <victor.90braz@gmail.com>
2024-01-19 17:25:21 +0100

    create a newslettercontroller

--- routes/web.php
+++ routes/web.php
@@ -33,23 +33,4 @@ Route::get('/members', function () {
     ddd($response);
 });
 
-Route::post('/newsletter', function (\App\Services\Newsletter $newsletter) {
-
-    request()->validate([
-        'email' => ['required', 'email']
-    ]);
-
-    try {
-
-        $newsletter->subscribe(request('email'));
-
-        return redirect('/')->with('success', 'You are subscribed now.');
-
-    }catch (\Exception $e) {
-
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter.'
-        ]);
-    }
-
-});
+Route::post('/newsletter', [\App\Http\Controllers\NewsletterController::class, 'create']);
