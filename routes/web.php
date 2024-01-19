1e7294d68897f41ba4930472206d0b2b923fc4f8
victor braz <victor.90braz@gmail.com>
2024-01-19 16:07:31 +0100

    create a newsletter route

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,6 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us21'
-    ]);
-
-    $response = $mailchimp->lists->addListMember("76cf69a4f6", [
-        'email_address' => 'victor.90braz@gmail.com',
-        'status' => 'subscribed'
-    ]);
-
-    ddd($response);
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
@@ -36,3 +19,34 @@ Route::post('login', [SessionController ::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionController ::class, 'destroy'])->middleware('auth');
 
+Route::get('/members', function () {
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->getListMembersInfo("76cf69a4f6");
+    ddd($response);
+});
+
+Route::post('/newsletter', function () {
+
+    request()->validate(['email' => 'required|email']);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $mailchimp->lists->addListMember("76cf69a4f6", [
+        'email_address' => request('email'),
+        'status' => 'subscribed'
+    ]);
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter');
+});
